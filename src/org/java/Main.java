e1a4c161ff180b229ee93b23f2b544860c1a9cdd
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-09-19 16:00:29 +0200

    add write books list

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,10 +1,17 @@
 package org.java;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import org.java.book.Libro;
 
 public class Main {
+	
+	private static final String FILE_PATH = "C:\\esercizi_file\\libri.txt";
+
+	
 	public static void main(String[] args)  {
 		
 		Scanner sc= new Scanner(System.in);
@@ -40,16 +47,51 @@ public class Main {
 		
 		}
 		sc.close();
-		
-		for (int x=0;x<booksCount;x++) {
-			
-			
-			System.out.println("\nlibro " + (x + 1));
-			System.out.println("------------------");
-			System.out.println(libri[x]);
-			System.out.println("");
-			
+		FileWriter myWriter = null;
+
+		try {
+		    myWriter = new FileWriter(FILE_PATH);
+
+		    for (int x = 0; x < booksCount; x++) {
+		        myWriter.write("\nlibro " + (x + 1) + "\n");
+		        myWriter.write("------------------\n");
+		        myWriter.write(libri[x].toString() + "\n");
+		    }
+		} catch (IOException e) {
+		    System.out.println("Errore nell'apertura o scrittura del file: " + e.getMessage());
+		} finally {
+		    try {
+		        if (myWriter != null) {
+		            myWriter.close();
+		        }
+		    } catch (IOException e) {
+		        System.out.println("Errore nella chiusura del file: " + e.getMessage());
+		    }
 		}
+
+
+		
+
+				File tmpFile = new File(FILE_PATH);
+				Scanner reader = null;
+				try {
+					
+					reader = new Scanner(tmpFile);
+					
+					while (reader.hasNextLine()) {
+						
+						String line = reader.nextLine();
+						System.out.println(line);
+					}
+				} catch (Exception e) { 
+				
+					System.out.println("Error reading file: " + e.getMessage());
+				} finally {
+					
+					if (reader != null)
+						reader.close();
+				}
+			
 	}
 	
 }
